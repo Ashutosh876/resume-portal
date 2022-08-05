cc46a831a0020a7267e17b1fd2278f29d57ededc
Ashutosh876 <ashutosha876@gmail.com>
2022-08-06 03:00:21 +0530

    Rendering html content in browser

--- src/main/java/io/ashutosh/resumeportal/controller/HomeController.java
+++ src/main/java/io/ashutosh/resumeportal/controller/HomeController.java
@@ -1,9 +1,12 @@
 package io.ashutosh.resumeportal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
@@ -11,9 +14,15 @@ public class HomeController {
         return "Hello";
     }
 
-    @GetMapping("/edit")
-    public String edit() {
-        return "edit page";
+    @GetMapping("/edit/{userId}")
+    public String edit(@PathVariable String userId) {
+        return userId;
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 
 }
